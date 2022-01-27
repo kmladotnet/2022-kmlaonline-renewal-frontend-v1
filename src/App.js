ccc3a063e6cc230acf4e60c440cf5172c533e135
real-lake050317 <jinojoa317@naver.com>
2022-01-28 01:58:08 +0900

    Added Routers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 import MainScreen from './pages/MainScreen';
+import Profile from './pages/Profile';
 
 function App() {
   return (
-    <div className="App">
-      <MainScreen />
-    </div>
+    <Router>
+      <main>
+        <Switch>
+          <Route path="/user/id" exact>
+            <Profile />
+          </Route>
+          <Route path="" exact>
+            <MainScreen />
+          </Route>
+          <Redirect to="" />
+        </Switch>
+      </main>
+    </Router>
   );
 }
 
